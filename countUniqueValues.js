e755b31508ffc146efc8e5df99e48b85dd424cb0
Deepesh Kumar R <deepeshkumarr316@gmail.com>
2020-06-07 20:51:16 +0530

    countUniqueValues: check for empty array

--- countUniqueValues.js
+++ countUniqueValues.js
@@ -38,6 +38,9 @@ function countUniqueValuesFirstApproach(arr1) {
  */
 
 function countUniqueValuesSecondApproach(arr1) {
+  if (arr1.length === 0) {
+    return 0;
+  }
   let i = 0;
   for (let j = 1; j < arr1.length; j++) {
     if (arr1[i] !== arr1[j]) {
