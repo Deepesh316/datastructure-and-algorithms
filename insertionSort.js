d7baf7366f38dae57cebf7cf1dddb9eec3e8cf6e
Deepesh Kumar R <deepeshkumarr316@gmail.com>
2020-06-13 08:40:38 +0530

    add psuedocode for insertion sort alogrithm

--- insertionSort.js
+++ insertionSort.js
@@ -1,3 +1,10 @@
+/**
+ * step 1 : start by picking second element in the array
+ * step 2: compare the second element with one before it and swap if necessary.
+ * step 3: continue to the next element, if it is in the incorrect order, iterate
+ *         through sorted portion (ie; the left side) to place element in correct place.
+ * step 4: Repeat until the array is sorted
+ */
 function insertionSort(arr) {
   let j;
   for (let i = 1; i < arr.length; i++) {
